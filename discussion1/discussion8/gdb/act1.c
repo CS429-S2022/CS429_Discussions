a0c0fc020f81ea7adbc9524caefe3ecb47031454
AbbhinavJayaraman <69915342+AbbhinavJayaraman@users.noreply.github.com>
2022-03-24 21:15:27 -0500

    Removing discussion8 from discussion1 - mistake

--- discussion1/discussion8/gdb/act1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char** argv)
-{    
-    int ret = printf("%s\n", argv[argc-1]);
-    argv[0] = '\0'; // NOOP to force gcc to generate a callq instead of jmp
-    return ret;
-}
